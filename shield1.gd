8839de5c8411aba175e6b54ecd850462144f9dab
Vivek Thazhathattil <vivek.thazhathattil@gmail.com>
2020-06-17 20:24:00 +0530

    Changes to be committed: 	modified:   .import/icon.png-487276ed1e3a0c39cad0279d744ee560.md5 	modified:   .import/icon.png-487276ed1e3a0c39cad0279d744ee560.stex 	modified:   .import/level_change.ogg-514e62b35fc51964fd9dc7aeff84021f.md5 	modified:   .import/level_change.ogg-514e62b35fc51964fd9dc7aeff84021f.oggstr 	modified:   assets/level_change.ogg.import 	modified:   icon.png 	modified:   menu.tscn 	modified:   player.tscn 	modified:   project.godot 	modified:   shield1.gd 	modified:   shield1.tscn 	modified:   store.tscn

--- shield1.gd
+++ shield1.gd
@@ -4,11 +4,11 @@ var shield_texture = preload("res://sprites/shield1-des7.png")
 
 func _ready():
 	$shield.set_texture(shield_texture)
-	pass # Replace with function body.
+	self.set_angular_velocity(10)
 
 func _custom_rotate():
-	$AnimationPlayer.play("shield_rotate")
-#	pass
+#	$AnimationPlayer.play("shield_rotate")
+	pass
 func _custom_rotate_backwards():
-	$AnimationPlayer.play_backwards("shield_rotate")
-#	pass
+#	$AnimationPlayer.play_backwards("shield_rotate")
+	pass
